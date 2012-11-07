9a39bb7e1fc77d878017bc363c2f5947dd5decb2
studywolf <travis.dewolf@gmail.com>
2012-11-07 11:30:25 -0800

    added 'neurons' parameter to BG template, changed 'D' and 'N' to 'dimensions' and 'neurons' in /nps/basalganglia.py file

--- python/nef/templates/basalganglia.py
+++ python/nef/templates/basalganglia.py
@@ -22,13 +22,13 @@ import numeric
 from ca.nengo.model.impl import NetworkImpl
 from java.util import ArrayList
 from java.util import HashMap
-def make(net,name='Basal Ganglia',dimensions=1,pstc=0.01,netbg=None,same_neurons=True):
+def make(net,name='Basal Ganglia',dimensions=1, neurons=100,pstc=0.01,netbg=None,same_neurons=True):
 
     if netbg is None:
         netbg=nef.Network(name)
     input=netbg.make('input',1,dimensions,quick=True,mode='direct')
     output=netbg.make('output',1,dimensions,quick=True,mode='direct')
-    nps.basalganglia.make_basal_ganglia(netbg,input,output,dimensions,same_neurons=same_neurons)
+    nps.basalganglia.make_basal_ganglia(netbg,input,output, dimensions=dimensions, neurons=neurons, same_neurons=same_neurons)
 
     input.addDecodedTermination('input',numeric.eye(dimensions),pstc,False)
     netbg.network.exposeTermination(input.getTermination('input'),'input')
